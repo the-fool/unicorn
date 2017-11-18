14d6cad38d8f63bfc1e114e4b1a419589dfaabee
thomas <sketchbang@gmail.com>
2017-11-17 20:09:53 -0800

    thinking button

--- static/dist/main.js
+++ static/dist/main.js
@@ -1,1 +1,43 @@
-"use strict";
+'use strict';
+
+function subscribe() {
+    var inputs = $('#subscribe-form input');
+    var name = inputs[0].value;
+    var email = inputs[1].value;
+    var data = {
+        name: name,
+        email: email
+    };
+
+    var button = $('#subscribe-form button');
+    button.text('Computing.');
+    var dots = 1;
+    var interval = setInterval(function () {
+        button.text('Computing' + '.'.repeat(dots));
+        dots = (dots + 1) % 4;
+    }, 250);
+    button.prop('disabled', true);
+    $.ajax({
+        method: "POST",
+        url: 'https://7zao5vcvl2.execute-api.us-west-2.amazonaws.com/prod/email',
+        data: JSON.stringify({
+            body: JSON.stringify(data),
+            subject: 'subscriber',
+            reply: 'admin@unicorncode.org'
+        }),
+        crossDomain: true,
+        contentType: 'application/json',
+        success: subscribeSuccess,
+        error: subscribeError,
+        complete: function complete() {
+            button.prop('disabled', false);
+        }
+    });
+}
+
+function subscribeSuccess() {
+    $('#contact').hide();
+    $('#thanks').show();
+}
+
+function subscribeError() {}
--- static/main.js
+++ static/main.js
@@ -6,6 +6,15 @@ function subscribe() {
         name,
         email
     };
+
+    const button = $('#subscribe-form button');
+  button.text('Computing.');
+  let dots = 1;
+  const interval = setInterval(() => {
+    button.text(`Computing${'.'.repeat(dots)}`);
+    dots = (dots + 1) % 4;
+  }, 250)
+    button.prop('disabled', true);
     $.ajax({
         method: "POST",
         url: 'https://7zao5vcvl2.execute-api.us-west-2.amazonaws.com/prod/email',
@@ -15,6 +24,20 @@ function subscribe() {
             reply: 'admin@unicorncode.org'
         }),
         crossDomain: true,
-        contentType: 'application/json'
+        contentType: 'application/json',
+        success: subscribeSuccess,
+        error: subscribeError,
+        complete: function() {
+            button.prop('disabled', false);
+        }
     });
+}
+
+function subscribeSuccess() {
+  $('#contact').hide();
+  $('#thanks').show();
+}
+
+function subscribeError() {
+  
 }
